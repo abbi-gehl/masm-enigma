2bfc04d841fe0039b9a84ac55948bf6c847ed5e5
Harrison Gehlbach <harrison.gehlbach@gmail.com>
2023-11-28 22:20:11 -0500

    adding file IO to the cpp file, updaing comments and formatting for asm

--- final_project/final_project/main.cpp
+++ final_project/final_project/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
-using namespace std;
-#include <bitset>
 #include <string>
-template<> struct hash<char>;
+#include <fstream>
+#include <string>
+
+
+using namespace std;
 
 extern "C" void _asmMain();
 
@@ -17,6 +19,44 @@ extern "C" string _getString() {
 	return s;
 }
 
+/*File I/O by Abbi Gehlbach */
+extern "C" void _writeFile(string input, int mode) {
+	ofstream file;
+
+	if (mode == 0) {
+		file.open("input.txt", ios::out);
+	}
+
+	if (mode != 0) {
+		file.open("output.txt", ios::out);
+	}
+
+	file << input;
+
+	file.close();
+
+}
+
+extern "C" string _readFile(int mode) {
+	ifstream file;
+	string output = "";
+	string line;
+
+	if (mode == 0) {
+		file.open("input.txt", ios::in);
+	}
+	if (mode != 0) {
+		file.open("output.txt", ios::in);
+	}
+
+	if (file.is_open()) {
+		while (file.good()) {
+			getline(file, line);
+		}
+	}
+	file.close();
+	return line;
+}
 
 // main stub driver
 int main() {
