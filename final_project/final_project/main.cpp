f6b8b78908e42c58ae64d532e5c30a9563897995
abbi gehlbach <harrison.gehlbach@gmail.com>
2023-12-10 19:12:00 -0500

    Adding FILE IO to asm and removing old IO from C++

--- final_project/final_project/main.cpp
+++ final_project/final_project/main.cpp
@@ -13,78 +13,13 @@ extern "C" void _printString(char* s) {
 	return;
 }
 
-extern "C" string _getString() {
-	string s;
-	cin >> s;
-	return s;
-}
-
-/*File I/O by Abbi Gehlbach */
-extern "C" void _writeFile(string input, int mode) {
-	ofstream file;
-
-	if (mode == 0) {
-		file.open("input.txt", ios::out);
-	}
-
-	if (mode != 0) {
-		file.open("output.txt", ios::out);
-	}
-
-	file << input;
-
-	file.close();
-
-}
-
-extern "C" void _readFile(char* buffer,int mode) {
-	ifstream file;
-	const char* charArr = {};
-
-	if (mode == 0) {
-		file.open("input.txt", ios::in);
-	}
-	if (mode != 0) {
-		file.open("output.txt", ios::in);
-	}
-
-	if (file.is_open()) {
-		while (!file.eof()) {
-			file >> buffer;
-		}
-	}
-	file.close();
-
+extern "C" void _getString(char * buffer, int num) {
+	cin.getline(buffer, num);
 	return;
 }
 
-// Harrison B - Clears file depending on the mode of use (input or output)
-extern "C" void _clearFile(int mode) {
-	ofstream file;
-
-	if (mode == 0) {
-		file.open("input.txt", ios::trunc);
-	}
-	if (mode != 0) {
-		file.open("output.txt", ios::trunc);
-	}
-
-	if (file.is_open()) {
-		if (file.good())
-		{
-			cout << "\nFile Cleared!\n";
-		}
-		else
-		{
-			cout << "\nFile Unable to be cleared!\n";
-		}
-	}
-	else
-	{
-		cout << "\nFile Unable to be cleared!\n";
-	}
-	file.close();
-	return;
+extern "C" void _newLine() {
+	cout << endl;
 }
 
 // main stub driver
